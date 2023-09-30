45017498f1e6b17d0768e180d111de436add06b0
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-09-30 10:58:33 -0300

    Criando metodo de mapear delete

--- src/main/java/dio/web/api/repository/UsuarioRepository.java
+++ src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -20,12 +20,12 @@ public class UsuarioRepository {
         System.out.println(usuario);
     }
 
-    public void remove(Integer id) {
+    public void deleteById(Integer id) {
         System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
         System.out.println(id);
     }
 
-    public List<Usuario> listAll() {
+    public List<Usuario> findAll() {
         System.out.println("LIST - Listando os usários do sistema");
         List<Usuario> usuarios = new ArrayList<>();
         usuarios.add(new Usuario("gleyson", "password"));
@@ -33,9 +33,15 @@ public class UsuarioRepository {
         return usuarios;
     }
 
-    public Usuario findById(Integer id) { // Correção aqui
+    public Usuario findById(Integer id) {
         System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
         return new Usuario("gleyson", "password");
     }
+
+    public Usuario findByUsername(String username) {
+        System.out.println(String.format("FIND/username - Recebendo o username: %s para localizar um usuário", username));
+        return new Usuario("gleyson", "password");
+    }
+
 }
 

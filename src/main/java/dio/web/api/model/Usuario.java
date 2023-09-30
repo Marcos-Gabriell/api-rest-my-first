0f04576c0b2e7c1f080c9ccdc6ab0975d7915551
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-09-29 23:37:03 -0300

    Terminado o model

--- src/main/java/dio/web/api/model/Usuario.java
+++ src/main/java/dio/web/api/model/Usuario.java
@@ -2,4 +2,45 @@ package dio.web.api.model;
 
 public class Usuario {
 
+    private Integer id;
+    private String login;
+    private String passoword;
+
+    public Usuario() {}
+    public Usuario (String login, String passoword) {
+        this.login = login;
+        this.passoword = passoword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", passoword= ' " + passoword + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassoword() {
+        return passoword;
+    }
+
+    public void setPassoword(String passoword) {
+        this.passoword = passoword;
+    }
 }
